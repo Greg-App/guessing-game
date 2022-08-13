600d532527a820e4f051d07fee6579e69016229b
Greg App <greg358@mail.ru>
2022-08-13 23:37:35 +0300

    feat: make first try solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,38 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.result =null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let arr = [];
+        for (let i = this.min; i <= this.max; i++) {
+            arr.push(i);
+        }
+        this.result = arr[Math.floor(arr.length/2)];
+      /* if (this.result>number) {
+        this.lower();
+        this.guess();
+      }
+      if (this.result<number) {
+        this.greater();
+        this.guess();
+      } */
+      return this.result;
     }
 
     lower() {
-
+        this.max =this.result;
     }
 
     greater() {
-
+        this.min =this.result;
     }
 }
 
